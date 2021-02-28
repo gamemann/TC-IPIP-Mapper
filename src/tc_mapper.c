ef7c1eff8b18ff1ef858189265e08c341486e477
gamemann <gamemann@gflclan.com>
2021-02-28 15:52:26 +0000

    Only define what we need.

--- src/tc_mapper.c
+++ src/tc_mapper.c
@@ -12,20 +12,13 @@
 
 #include <bpf_helpers.h>
 
-#define likely(x) __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #define htons(x) ((__be16)___constant_swab16((x)))
-#define ntohs(x) ((__be16)___constant_swab16((x)))
-#define htonl(x) ((__be32)___constant_swab32((x)))
-#define ntohl(x) ((__be32)___constant_swab32((x)))
 #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
 #define htons(x) (x)
-#define ntohs(X) (x)
-#define htonl(x) (x)
-#define ntohl(x) (x)
 #endif
-
+#define offsetof(TYPE, MEMBER) ((uint16_t)&((TYPE *)0)->MEMBER)
 // TC has its own map definition.
 struct bpf_elf_map 
 {
