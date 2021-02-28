9776fe39aa3f55e8dd18dc13dfe317a9103c86a6
gamemann <gamemann@gflclan.com>
2021-02-28 15:37:46 +0000

    Complete TC Mapper program (Not Tested).

---
+++ src/tc_mapper.c
@@ -0,0 +1,99 @@
+#include <linux/bpf.h>
+#include <linux/pkt_cls.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/udp.h>
+#include <linux/tcp.h>
+#include <linux/icmp.h>
+#include <linux/ip.h>
+#include <linux/in.h>
+#include <linux/types.h>
+#include <inttypes.h>
+
+#include <bpf_helpers.h>
+
+#ifdef __BPF__
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#define htons(x) ((__be16)___constant_swab16((x)))
+#define ntohs(x) ((__be16)___constant_swab16((x)))
+#define htonl(x) ((__be32)___constant_swab32((x)))
+#define ntohl(x) ((__be32)___constant_swab32((x)))
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#define htons(x) (x)
+#define ntohs(X) (x)
+#define htonl(x) (x)
+#define ntohl(x) (x)
+#endif
+#endif
+
+// TC has its own map definition.
+struct bpf_elf_map 
+{
+    uint32_t type;
+    uint32_t size_key;
+    uint32_t size_value;
+    uint32_t max_elem;
+    uint32_t flags;
+    uint32_t id;
+    uint32_t pinning;
+};    
+
+struct bpf_elf_map SEC("maps") mapping =
+{
+    .type = BPF_MAP_TYPE_LRU_HASH,
+    .size_key = sizeof(uint32_t),
+    .size_value = sizeof(uint32_t),
+    .max_elem = 10000,
+    .pinning = 2
+};
+
+SEC("mapper")
+int mapper_prog(struct __sk_buff *skb)
+{
+    // Initialize packet data.
+    void *data_end = (void *)(long)skb->data_end;
+    void *data = (void *)(long)skb->data;
+
+    // Initialize Ethernet header.
+    struct ethhdr *eth = data;
+
+    if (unlikely(eth + 1 > (struct ethhdr *)data_end))
+    {
+        return TC_ACT_SHOT;
+    }
+
+    // If not IPv4, pass along.
+    if (eth->h_proto != htons(ETH_P_IP))
+    {
+        return TC_ACT_OK;
+    }
+
+    // Initialize (outer) IP header.
+    struct iphdr *oiph = data + sizeof(struct ethhdr);
+
+    if (unlikely(oiph + 1 > (struct iphdr *)data_end))
+    {
+        return TC_ACT_SHOT;
+    }
+
+    // If the IP protocol isn't IPIP, pass along.
+    if (oiph->protocol != IPPROTO_IPIP)
+    {
+        return TC_ACT_OK;
+    }
+
+    // Initialize (inner) IP header.
+    struct iphdr *iph = data + sizeof(struct ethhdr) + (oiph->ihl * 4);
+
+    if (unlikely(iph + 1 > (struct iphdr *)data_end))
+    {
+        return TC_ACT_SHOT;
+    }
+
+    // Simply map the client IP (key) to the incoming remote IP (value).
+    bpf_map_update_elem(&mapping, &iph->saddr, &oiph->saddr, BPF_ANY);
+
+    return TC_ACT_OK;
+}
