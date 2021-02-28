4ef288f601255377095e078fa0bf812609f734ec
gamemann <gamemann@gflclan.com>
2021-02-28 14:59:05 +0000

    Add command line support.

--- src/ipipmapper.c
+++ src/ipipmapper.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "cmdline.h"
 
 int main(int argc, char *argv[])
 {
+    // Parse the command line and retrieve the interface we want to attach the TC programs to.
+    struct cmdline cmd = {0};
 
+    parsecmdline(argc, argv, &cmd);
+
+    // Check to ensure interface isn't NULL.
+    if (cmd.dev == NULL)
+    {
+        fprinf(stderr, "No interface specified. Pleasse specify an interface with the -i or --dev flag.\n");
+
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
