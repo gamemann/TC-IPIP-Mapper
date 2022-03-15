424a9e57a51a3a8eecbb111f4b9eb48ea84ef8c0
gamemann <gamemann@gflclan.com>
2022-03-15 23:01:40 +0000

    Add debugging.

--- src/tc_mapper.c
+++ src/tc_mapper.c
@@ -21,6 +21,7 @@
 
 //#define REPLACE_SOURCE_REMOTE 0
 //#define REPLACE_SOURCE_WITH_INNER
+//#define DEBUG
 
 // TC has its own map definition.
 struct bpf_elf_map 
@@ -99,6 +100,10 @@ int mapper_prog(struct __sk_buff *skb)
     // Simply map the client IP (key) to the incoming remote IP (value).
     bpf_map_update_elem(&mapping, &iph->saddr, &oiph->saddr, BPF_ANY);
 
+#ifdef DEBUG
+    bpf_printk("[TC_MAPPER] Mapping client %lu to %lu.\n", iph->saddr, oiph->saddr);
+#endif
+
 #ifdef REPLACE_SOURCE_REMOTE
     // Replace the outer IP header's source address with this.
     __be32 oldremote = oiph->saddr;
@@ -107,7 +112,11 @@ int mapper_prog(struct __sk_buff *skb)
     #else
     oiph->saddr = REPLACE_SOURCE_REMOTE;
     #endif
-    
+
+#ifdef DEBUG
+    bpf_printk("[TC_MAPPER] Replacing source IP %lu with %lu.\n", oldremote, oiph->saddr);
+#endif    
+
     bpf_l3_csum_replace(skb, (sizeof(struct ethhdr) + offsetof(struct iphdr, daddr)), oldremote, oiph->daddr, sizeof(oiph->daddr));
 #endif
 
