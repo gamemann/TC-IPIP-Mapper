d4fad123828f0f8a0206cb0320b842a10e427c86
gamemann <gamemann@gflclan.com>
2022-03-15 22:53:44 +0000

    Also support for replacing with inner IP header dest IP.

--- src/tc_mapper.c
+++ src/tc_mapper.c
@@ -20,6 +20,7 @@
 #endif
 
 //#define REPLACE_SOURCE_REMOTE 0
+//#define REPLACE_SOURCE_WITH_INNER
 
 // TC has its own map definition.
 struct bpf_elf_map 
@@ -101,8 +102,11 @@ int mapper_prog(struct __sk_buff *skb)
 #ifdef REPLACE_SOURCE_REMOTE
     // Replace the outer IP header's source address with this.
     __be32 oldremote = oiph->saddr;
-
+    #ifdef REPLACE_SOURCE_WITH_INNER
+    oiph->saddr = iph->daddr;
+    #else
     oiph->saddr = REPLACE_SOURCE_REMOTE;
+    #endif
     
     bpf_l3_csum_replace(skb, (sizeof(struct ethhdr) + offsetof(struct iphdr, daddr)), oldremote, oiph->daddr, sizeof(oiph->daddr));
 #endif
